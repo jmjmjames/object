ed9e40b720fa6f8a06f8996e4e346171b7539c2d
jmjmjames <chungjm0711@gmail.com>
2022-12-10 03:12:00 +0900

    Seller가 ticket을 파는 것이다.

--- src/main/java/object/chap01/TicketSeller.java
+++ src/main/java/object/chap01/TicketSeller.java
@@ -1,6 +1,7 @@
 package object.chap01;
 
 public class TicketSeller {
+
     private final TicketOffice ticketOffice;
 
     public TicketSeller(TicketOffice ticketOffice) {
@@ -9,5 +10,22 @@ public class TicketSeller {
 
     public TicketOffice getTicketOffice() {
         return ticketOffice;
+    }
+
+    /**
+     * Theater가 파는 것이 아닌 Seller가 파는 것이다.
+     *
+     * @param audience
+     */
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
