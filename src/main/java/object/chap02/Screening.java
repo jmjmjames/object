5fc0330094c1ba77d183b72c9bad77c9dc004950
jmjmjames <chungjm0711@gmail.com>
2022-12-10 23:50:23 +0900

    feat: Screening 의인화하여 reserve 한다 reservation
    customer, count 로

--- src/main/java/object/chap02/Screening.java
+++ src/main/java/object/chap02/Screening.java
@@ -3,7 +3,6 @@ package object.chap02;
 import java.time.LocalDateTime;
 
 public class Screening {
-
     private final Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
@@ -14,6 +13,16 @@ public class Screening {
         this.whenScreened = whenScreened;
     }
 
+    // customer, audienceCount 만큼 상영을 예매함
+    // 예매를 할 때 1인당 예매 요금 * cnt
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
     public Money getMovieFee() {
         return movie.getFee();
     }
@@ -22,7 +31,11 @@ public class Screening {
         return sequence;
     }
 
-    public LocalDateTime getWhenScreened() {
+    public LocalDateTime getStartTime() {
         return whenScreened;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
     }
 }
