49eaa5af5b7d6be778b87731970af8ad1a87a1f0
jmjmjames <chungjm0711@gmail.com>
2022-12-10 23:57:37 +0900

    feat: 영화는 할인 정책과 영화 제목, 정가, runningTime
    - 영화 가격은 Screening의 협력을 통해 정해짐

--- src/main/java/object/chap02/Movie.java
+++ src/main/java/object/chap02/Movie.java
@@ -1,12 +1,26 @@
 package object.chap02;
 
+
+import java.time.Duration;
+
 public class Movie {
+    private final String title;
+    private final Duration runningTime;
+    private final Money fee;
+    private final DiscountPolicy discountPolicy;
 
-    private int fee;
-
-
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
 
     public Money getFee() {
-        return Money(fee);
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
 }
